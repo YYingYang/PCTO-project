8b21339c30ba25067c6395b98d594effdaba96ba
Giacomo Yang <giacomo.gy@gmail.com>
2021-06-15 16:56:43 +0200

    fix: aggiunta funzione rirca e miglioramenti

--- src/App.js
+++ src/App.js
@@ -1,52 +1,69 @@
 class App {
-  constructor() {}
-}
-
-const app = new App();
-
-class film {
-  constructor(titolo, genere, anno) {
-    this.titolo = titolo;
-    this.genere = genere;
-    this.anno = anno;
+  constructor(list) {
+    this.list=list
+    this.display(this.list)
   }
-  titolo;
-  genere;
-  anno;
+  display(list) {
+    let obj = document.getElementById("list");
+    let ul = document.createElement("ul");
+    for (let i = 0; i < list.length; i++) {
+      let li = ul.appendChild(document.createElement("li"));
+      li.classList.add("list-group-item");
+      li.appendChild(
+        document.createTextNode(
+          "Film " + i + ": " + list[i].movieToString()
+        )
+      );
+      ul.appendChild(li);
+    }
+    obj.appendChild(ul);
+  }
+  
+  ricerca() {
+    let str = document.getElementById("search").value;
+    let temp=[];
+    for (let i = 0; i < this.list.length; i++) {
+      if(this.list[i].title.toLowerCase().includes(str.toLowerCase()))
+        temp.push(this.list[i])
+    }
+    if(temp.length==0)
+      document.getElementById("list").innerHTML = "nulla";
+    else{
+      document.getElementById("list").innerHTML=""  
+      this.display(temp)
+    } 
+    document.getElementById("search").value=""
+  }
+  
 }
 
-var catalogo = [];
-
-catalogo.push(new film("F&F1", "azione", "2000"));
-catalogo.push(new film("Amore e OdioF&F1", "romantico", "2002"));
-catalogo.push(new film("2030", "scientifico", "2004"));
-catalogo.push(new film("Pippo e Pluto", "giallo", "2005"));
-catalogo.push(new film("L'altro mondo", "fantasy", "2006"));
-catalogo.push(new film("La casa Stregata", "horror", "2010"));
-catalogo.push(new film("Corsa a tre", "azione", "2009"));
-catalogo.push(new film("WWII", "storico", "1999"));
-catalogo.push(new film("Sherlock on the moon", "poliziesco", "1899"));
-catalogo.push(new film("Giungla13", "fiction", "2021"));
-
-var obj = document.getElementById("list");
-var ul = document.createElement("ul"); //creiamo l'elemento ul nel DOM
-// ul.classList.add("lista")
-//ul.classList.add("list-group list-group-flush")
-for (let i = 0; i < catalogo.length; i++) {
-  let li = ul.appendChild(document.createElement("li"));
-  li.classList.add("list-group-item");
-  li.appendChild(
-    document.createTextNode(
-      "Film " +
-        i +
-        ": " +
-        catalogo[i].titolo +
-        ", " +
-        catalogo[i].genere +
-        ", " +
-        catalogo[i].anno
-    )
-  );
-  ul.appendChild(li);
+class Movie {
+  constructor(title, category, year) {
+    this.title = title;
+    this.category = category;
+    this.year = year;
+  }
+  movieToString(){
+    let str = this.title + ", " + this.category + ", " + this.year
+    return str;
+  }
 }
-obj.appendChild(ul);
+
+const list=[
+  new Movie("F&F1", "azione", "2000"),
+  new Movie("Amore e Odio", "romantico", "2002"),
+  new Movie("2030", "scientifico", "2004"),
+  new Movie("Pippo e Pluto", "giallo", "2005"),
+  new Movie("L'altro mondo", "fantasy", "2006"),
+  new Movie("La casa Stregata", "horror", "2010"),
+  new Movie("Corsa a tre", "azione", "2009"),
+  new Movie("WWII", "storico", "1999"),
+  new Movie("Sherlock on the moon", "poliziesco", "1899"),
+  new Movie("Giungla13", "fiction", "2021"),
+]
+
+const app = new App(list);
+
+
+
+
